b4f8135a93ea4b4ec6771653280d5c7fb1767fc6
Prakhar Gupta <prakhar.g@students.iiit.ac.in>
2022-09-01 16:53:23 +0530

    Lab 4 final submission

---
+++ ssd_lab_activity_4/2022202021/q4.sql
@@ -0,0 +1,33 @@
+delimiter //
+CREATE PROCEDURE IF NOT EXISTS retrieve() 
+BEGIN
+    DECLARE cust_name varchar(255) DEFAULT "";
+    DECLARE cust_city varchar(255) DEFAULT "";
+    DECLARE cust_country varchar(255) DEFAULT "";
+    DECLARE cust_grade INT DEFAULT 0;
+    DECLARE ans varchar(255) DEFAULT "";
+    DECLARE numRows int DEFAULT 0;
+    
+    DECLARE ptr
+            CURSOR FOR 
+                SELECT CUST_NAME, CUST_CITY, CUST_COUNTRY, GRADE from customer where AGENT_CODE like 'A00%';
+    
+    OPEN ptr;
+
+    SET numRows = 25;
+
+    custLoop: 
+    LOOP
+        IF numRows > 0 THEN
+            FETCH ptr INTO cust_name, cust_city, cust_country, cust_grade;
+            SET ans = CONCAT(ans, cust_name, cust_city, cust_country, cust_grade);
+            SET numRows = numRows - 1;
+            Select ans;
+            SET ans = "";
+        ENF IF;
+        GOTO custLoop;
+    END LOOP;
+END;//
+
+delimiter ;
+CALL retrieve();
